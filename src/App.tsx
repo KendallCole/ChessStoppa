2a4b82010d34d97e348ab7d5eeadbef9eae05f3f
klc <its0me0klc@gmail.com>
2024-08-02 19:22:44 -0500

    almost ready...

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,223 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+//https://github.com/Hyper-Dragon/DgtAngel/blob/main/sourceExt/DgtAngelCdcPlay/src/js/inject/cdcPlayBoard.js
 
 
-function Main() {
+function main() {
+  const DEFAULT_COLOR = document.body.style.backgroundColor
 
-  console.log("Can I see the document?", document)
- 
-  const elements = document.querySelectorAll(".clock-component");
 
-  //@TODO Use this as an example message
-  chrome.runtime.sendMessage({ type: 'TIME_CHANGED', payload: "WE ARE SO BACK IT WORKS" });
+  // Create the dialog box
+  const dialogBox = document.createElement('div');
+  dialogBox.style.backgroundColor = 'white';
+  dialogBox.style.padding = '5px';
+  dialogBox.style.borderRadius = '8px';
+  dialogBox.style.textAlign = 'center';
+  dialogBox.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+  dialogBox.style.height = '150px'
+
+
+  const timeBetweenTurns = document.createElement('input')
+  timeBetweenTurns.id = 'tbt'
+  timeBetweenTurns.textContent = '5'
+  timeBetweenTurns.type = 'range'
+  timeBetweenTurns.min =  "1" 
+  timeBetweenTurns.value =  "5" 
+  timeBetweenTurns.max =  "30"
+  dialogBox.appendChild(timeBetweenTurns)
   
-  let observerList: any = []
-  elements.forEach((el) => {
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-          console.log('Time changed:', mutation.target.textContent);
-      });
-    });
 
-      observer.observe(el, { characterData: true, subtree: true, childList: true });
-      observerList.push(observer)
-      console.log("Observer hooked up", el)
-  });
+  const rangeValueDisplay = document.createElement('p')
+  rangeValueDisplay.id = 'countdownText'
+  rangeValueDisplay.style.fontSize = '24px'
+  rangeValueDisplay.style.color = '#a78989'
+  rangeValueDisplay.style.paddingBottom = '10px'
+  rangeValueDisplay.textContent = 'Chess Stopa Active'
+  dialogBox.appendChild(rangeValueDisplay);
+
+
+  const countdownText = document.createElement('p');
+  countdownText.id = 'countdownText';
+  countdownText.style.fontSize = '24px';
+  countdownText.style.color = '#333';
+  countdownText.textContent = 'Chess Stopa Active';
+  dialogBox.appendChild(countdownText);
+
+  function updateRangeValue() {
+    rangeValueDisplay.textContent = `Freeze time: ${timeBetweenTurns.value}`;
+  }
+
+  // Listen for the input event on the range input
+  timeBetweenTurns.addEventListener('input', updateRangeValue);
+
+  // Initialize the display with the default range value
+  updateRangeValue();
+
+
+
+  // Append dialogBox to overlay
+
+  // Append overlay to the body
+  document.body.insertBefore(dialogBox, document.body.firstChild);
+
+
+  const getPlayerName = (): string => {
+    const bottomBar = document.querySelector('.board-layout-bottom')
+    if(bottomBar){
+      const userTagline: any = bottomBar.querySelector('.user-tagline-username')
+      return userTagline.innerText.trim()
+    }
+    return "[player name not found]"
+  }
+  
+  const getMyClock = () => {
+    const myPlayerName = getPlayerName()
+    const whiteClock = document.getElementsByClassName("clock-white")[0]
+    const blackClock = document.getElementsByClassName("clock-black")[0]
+    //@ts-ignore
+    if(whiteClock.parentNode.parentNode.querySelector('.user-username-component').innerText.trim() == myPlayerName){
+      return [whiteClock, blackClock]
+    }else{
+      return [blackClock, whiteClock]
+    }   
+  }
+
+  const getTimeFromClock = (clock: any): number => {
+    
+    const time = clock.querySelector('.clock-time-monospace').innerText
+    const [minutes, seconds] = time.split(':')
+
+    // Convert both parts to numbers
+    const totalMinutes = parseInt(minutes, 10)
+    const totalSeconds = parseInt(seconds, 10)
+
+    // Calculate total seconds
+    const result = totalMinutes * 60 + totalSeconds
+
+    return result
+    
+
+  }
+  
+  let myMinTime = Infinity
+  const freezePlayerBoard = () => {
+    
+    console.log("My turn: FORCING PLAYER TO WAIT TO MOVE", console.log((myClock)))
+    const remainingTime = getTimeFromClock(myClock)
+    
+    if(remainingTime >= myMinTime){
+      console.log("ITS NOT MY MOSt RECENT TURN")
+      return
+    } 
+    console.log("It is my most recent turn!")
+    myMinTime = Math.min(myMinTime, remainingTime)
+
+    console.log("Time remaining: ", remainingTime)
+    if(remainingTime < 120){
+      console.log("Less than 1 min remaining, skipping timeout")
       
-  return observerList
+    }else{
+      let countdown = parseInt(timeBetweenTurns.value) -1
+
+      document.body.style.backgroundColor = 'grey'
+      countdownText.innerText = `${countdown + 1}`
+      document.documentElement.style.setProperty('pointer-events', 'none')
+
+
+  
+      const timer = setInterval(() => {
+        if (countdown > 0) {
+          countdownText.innerText = `${countdown}` 
+          countdown--
+        } else {
+          countdownText.innerText = "u can play :) uwu"
+          document.documentElement.style.setProperty('pointer-events', 'auto')
+          document.body.style.backgroundColor = DEFAULT_COLOR
+          clearInterval(timer); // Stop the interval
+        }
+      }, 1000); // 1000 ms = 1 second
+
+
+
+    
+    }
+  }
+
+ 
+  const [myClock, opponentClock] = getMyClock();
+  console.log("My clock?", myClock)
+  console.log("Opponent clock?", opponentClock)
+
+
+  const turnChangeChecker = document.querySelector('.clock-top')
+  const turnObserver = new MutationObserver((mutations) => {
+    mutations.forEach((_) => {
+      console.log("TURNS CHANGED")
+      if (myClock.classList.contains("clock-player-turn")) {
+        freezePlayerBoard()
+      }
+    });
+  })
+  //@ts-ignore
+  turnObserver.observe(turnChangeChecker, {
+    attributes: true,
+    characterData: true,
+    subtree: false,
+    childList: false
+  })
+  
+  // const opponentTimeObserver = new MutationObserver((mutations) => {
+  //   mutations.forEach((mutation) => {
+  //     console.log('His Time changed:', mutation.target.textContent);
+  //     //@TODO Freeze board for X amount of time.
+      
+      
+  //    
+    
+  //   });
+  // });
+
+  // const myTimeObserver = new MutationObserver((mutations) => {
+  //   mutations.forEach((mutation) => {
+  //     console.log('My Time changed:', mutation.target.textContent);
+  //   });
+  // });
+
+  // // Make sure the observer is observing outside the loop
+  
+  // //@ts-ignore
+  // opponentTimeObserver.observe(opponentClock.querySelector('.clock-time-monospace'), {
+  //   attributes: true,
+  //   characterData: true,
+  //   subtree: true,
+  //   childList: true
+  // })
+
+  // //@ts-ignore
+  // myTimeObserver.observe(myClock.querySelector('.clock-time-monospace') , {
+  //   attributes: true,
+  //   characterData: true,
+  //   subtree: true,
+  //   childList: true
+  // })
+      
+  chrome.runtime.sendMessage({ type: 'TIME_CHANGED', payload: "WE ARE SO BACK IT WORKS" });
+
+  return 
 }
 function App() {
-  const [count, setCount] = useState(0)
   const [_time, setTime] = useState('');
 
 
   const loadPage = async() => {
-    console.log("Begin async")
     let [tab] = await chrome.tabs.query({active: true});
     chrome.scripting.executeScript({
       target: {tabId: tab.id!},
-      func: Main
+      func: main
     
     //   func:  () => {
-    //     const chatWindow: any = document.querySelector('.game-start-message-component'); // Updated class name
-    //     console.log(chatWindow.childNodes[4].childNodes[0].innerText.trim(), " DID I GET THE USERNAME????")
-
-    //     const usernameElement: any = document.querySelector('.user-tagline-username'); // Updated class name
-    //     if(usernameElement != null){
-          
-    //       const username = usernameElement != null ? usernameElement.textContent.trim() : 'Username not found';
-    //       console.log('Username:', username);
-    //     }
         
 
-    //     //https://github.com/Hyper-Dragon/DgtAngel/blob/main/sourceExt/DgtAngelCdcPlay/src/js/inject/cdcPlayBoard.js
-    //     let whiteClock = document.getElementsByClassName("clock-white")[0];
-
-    //     whiteClock
-    //     console.log("White clock:")
-    //     console.log(whiteClock)
-    //     // blackClock = document.getElementsByClassName("clock-black")[0];
     //     // turn = turnCodes.NONE;
 
     //     // // Use the clocks to detect the turn
@@ -136,13 +297,8 @@ function App() {
   return (
     <>
       <p>
-        Chess Stoppa
+        Chess Stoppa :D
       </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>     
     </>
   )
 }
